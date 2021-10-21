816527a6d18f8fc6788c6b5325dc840939b90a4d
OMEGA <leon.wai@live.com>
2021-10-21 23:00:22 +0630

    fixed

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, {useEffect, useState} from "react";
 import { LineChart, Line, CartesianGrid, XAxis } from 'recharts';
 import vaccinatedUserService from "../services/vaccinatedUser";
 
